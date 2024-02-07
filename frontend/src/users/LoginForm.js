aca179d31873fd8ecafa3073acabff850919168d
Bhuvaneswari630 <bhuvaneswarim630@gmail.com>
2024-02-07 10:32:34 -0500

    sign up and login assignment

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -17,7 +17,30 @@ function LoginForm() {
 
     async function handleSubmit(e) {
         e.preventDefault()
-       
+        try {
+        const response = await fetch(`http://localhost:5000/authentication/`, {
+        method: 'POST',
+        // credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+        })
+
+        const data = await response.json()
+
+        if (response.status === 200) {
+            setCurrentUser(data.user)
+            // console.log(data.token);
+            localStorage.setItem('token', data.token)
+            history.push(`/`)
+        } else {
+            setErrorMessage(data.message)
+        }
+    } catch (error) {
+        console.log('An error occurred:', error);
+        setErrorMessage('An error occurred, please try again')
+    }
 
     }
 
@@ -66,3 +89,4 @@ function LoginForm() {
 }
 
 export default LoginForm
+  
